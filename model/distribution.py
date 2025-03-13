e3ad78cb8555c726dd7f2a1652d5d6d57a3a5b96
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2025-03-13 13:17:03 -0500

    fix

--- model/distribution.py
+++ model/distribution.py
@@ -1,11 +1,14 @@
 class Distribution(object):
     def __init__(self):
-        raise NotImplementedError
+        self.fixed = False
 
     def items() -> list:
         raise NotImplementedError
 
     def sample(self) -> list:
+        raise NotImplementedError
+    
+    def prob_of_xi_before_S(self, xi, S):
         raise NotImplementedError
     
     def prob_of_fixed_unordered_prefix(self, items):
